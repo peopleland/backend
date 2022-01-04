1e3b0ec98b8ca494aa95934aad0d6dde7bae5908
fushang318 <fushang318@gmail.com>
2022-01-04 14:45:37 +0800

    change background fun

--- app/user/cmd/background/syncopenerrecordbackground.go
+++ app/user/cmd/background/syncopenerrecordbackground.go
@@ -2,18 +2,15 @@ package main
 
 import (
 	"context"
-	"log"
-	"time"
-
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
 func handler(_ context.Context, _ events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
-	for i := 0; i < 60; i++ {
-		log.Println(i)
-		time.Sleep(1 * time.Second)
-	}
+	//for i := 0; i < 60; i++ {
+	//	log.Println(i)
+	//	time.Sleep(1 * time.Second)
+	//}
 	return nil, nil
 }
 
