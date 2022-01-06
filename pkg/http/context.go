af6786f284384f188e809d8400af2edf53edc0d7
fushang318 <fushang318@gmail.com>
2022-01-06 12:02:36 +0800

    fix bug

--- pkg/http/context.go
+++ pkg/http/context.go
@@ -3,9 +3,6 @@ package http
 import (
 	"backend/pkg/http/binding"
 	"context"
-	"net/url"
-
-	"github.com/gorilla/mux"
 	"github.com/labstack/echo/v4"
 )
 
@@ -14,15 +11,6 @@ type Context struct {
 	Ctx context.Context
 }
 
-func (c *Context) Vars() url.Values {
-	raws := mux.Vars(c.Context.Request())
-	vars := make(url.Values, len(raws))
-	for k, v := range raws {
-		vars[k] = []string{v}
-	}
-	return vars
-}
-
 func (c *Context) BindQuery(v interface{}) error {
-	return binding.BindQuery(c.Vars(), v)
+	return binding.BindQuery(c.QueryParams(), v)
 }
